3f1fcbc3d2e14561be22a0c0e19c4bf7d62f6eb0
wenqingyu <yuwenqingisu@gmail.com>
2016-04-27 17:42:01 +0800

    add demo code into master

--- index.js
+++ index.js
@@ -6,6 +6,8 @@
 var WebSocket = require('faye-websocket'),
     ws        = new WebSocket.Client('wss://pro-ws.btcc.com/');
 
+var url = "ws://192.168.1.12:8080/orderbook";
+
 ws.on('open', function(event) {
     console.log('Websocket Open');
 });
@@ -31,15 +33,15 @@ ws.on('close', function(event) {
 ws.on('error', function(event){
     console.log('error', event);
 });
-
-// get orderbook request
-var quoteRequest = JSON.stringify({"MsgType":"QuoteRequest","QuoteType":"2","Symbol":"XBTCNY","Depth":9});
-
-//get trades request
-var tradeRequest = JSON.stringify({"MsgType":"GetTradesRequest","Count":"20","Symbol":"XBTCNY"});
-
-// get BPI Index request
-var BPIRequest = JSON.stringify({"MsgType":"QuoteRequest","QuoteType":"2","Symbol":"BPICNY","Depth":1}); // set depth = 1, there is no more depth than 1
-
-// put the request you need to send
-ws.send(quoteRequest);
+//
+//// get orderbook request
+//var quoteRequest = JSON.stringify({"MsgType":"QuoteRequest","QuoteType":"2","Symbol":"XBTCNY","Depth":100});
+//
+////get trades request
+//var tradeRequest = JSON.stringify({"MsgType":"GetTradesRequest","Count":"20","Symbol":"XBTCNY"});
+//
+//// get BPI Index request
+//var BPIRequest = JSON.stringify({"MsgType":"QuoteRequest","QuoteType":"2","Symbol":"BPICNY","Depth":1}); // set depth = 1, there is no more depth than 1
+//
+//// put the request you need to send
+//ws.send(quoteRequest);
